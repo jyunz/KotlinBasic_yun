359f99d0463a9ef0b3237a3784e54d4084217dee
Roy <J.jyun0525@gmail.com>
2022-08-02 16:15:48 +0900

    Initial commit

---
+++ app/src/main/java/com/class101/kotlinbasic_yun/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.class101.kotlinbasic_yun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        clickBtn.setOnClickListener {
+            // 중괄호 안에 있는 내용을 실행.
+            Log.d("메인화면", "버튼이 클릭됨")
+            Log.e("메인화면","에러관련 로그")
+        }
+    }
+}
